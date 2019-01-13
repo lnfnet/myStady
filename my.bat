eca2de10d207032d6168440992238219ae13ae27
lnfnet <lnfnet2005@163.com>
2019-01-14 07:17:38 +0800

    2019-01-14
    地方

---
+++ my.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\lnf\Desktop\python\code\django_project\lean\1_env\Scripts\learning_blog
+cmd /k  "C:\Users\lnf\Desktop\python\code\django_project\lean\1_env\Scripts\activate.bat"
