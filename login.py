c049c8fd5989dfb0aba93d155049310d2eade7e1
lnfnet <lnfnet2005@163.com>
2019-01-21 23:50:46 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,4 @@
 import easygui as g 
-import urllib
-import urllib2
 import json
 
 #以下弹出窗口获取用户输入的账号密码
@@ -31,7 +29,7 @@ html = response.read()
 vmlist=json.loads(html)
 vmarr=[]
 for vm in vmlist:
-        vmarr.append(vm.get('vmname'))
+    vmarr.append(vm.get('vmname'))
 
 #显示虚拟机列表让用户选择
 selvm=g.buttonbox("please select desktop?",choices=vmarr)
@@ -39,9 +37,9 @@ selvm=g.buttonbox("please select desktop?",choices=vmarr)
 #获取选择虚拟机的ID
 myvmid=""
 for vm in vmlist:
-        if vm.get('vmname')==selvm:
-                myvmid= vm.get('vmid')
-                break
+    if vm.get('vmname')==selvm:
+        myvmid= vm.get('vmid')
+        break
 
 #getvncport服务端程序根据虚拟机ID启动该虚拟机，并获取连接URL
 url="http://192.168.0.100:8000/getvncport/?ty=spice&vm=" + myvmid
